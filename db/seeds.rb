887dea4bdc6ea3ede44d1fa6bff5bf97a42f49cc
OGNeekoPolo <nktrabue@gmail.com>
2017-09-05 14:26:03 -0400

    Few tweaks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,438 +5,438 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# Owner.create(
-#   name: "Camden Allen"
-#   email: "allen376@purdue.edu"
-#   phone: "317-989-2713"
-# )
-#
-# Owner.create(
-#   name: "Jose Alvarado"
-#   email: "jalvarado22@ivytech.edu"
-#   phone: "317-941-5774"
-# )
-#
-# Owner.create(
-#   name: "Matheu Bragg"
-#   email: "matheubragg@gmail.com"
-#   phone: "513-461-5123"
-# )
-#
-# Owner.create(
-#   name: "Ryan Brown"
-#   email: "ryanwbrown_2001@icloud.com"
-#   phone: "317-385-6691"
-# )
-#
-# Owner.create(
-#   name: "Bailey Bolton"
-#   email: "boltonbailey@yahoo.com"
-#   phone: "317-970-3154"
-# )
-#
-# Owner.create(
-#   name: "Levon Brown"
-#   email: "levon.brown@yahoo.com"
-#   phone: "317-260-7059"
-# )
-#
-# Owner.create(
-#   name: "Andrew Bolen"
-#   email: "abolsax@yahoo.com"
-#   phone: "317-608-7714"
-# )
-#
-# Owner.create(
-#   name: "Brock Bishop"
-#   email: "brockjbishop@gmail.com"
-#   phone: "317-771-6651"
-# )
-#
-# Owner.create(
-#   name: "Dan Blazier"
-#   email: "dblazier1@gmail.com"
-#   phone: "618-973-9063"
-# )
-#
-# Owner.create(
-#   name: "William Coffee"
-#   email: "wcoffee05@yahoo.com"
-#   phone: "773-934-4770"
-# )
-#
-# Owner.create(
-#   name: "Aaron Cunningham"
-#   email: "brightfuture317@gmail.com"
-#   phone: "317-453-7188"
-# )
-#
-# Owner.create(
-#   name: "Courtnee Cruz"
-#   email: "courtnee.cruz@gmail.com"
-#   phone: "219-228-0178"
-# )
-#
-# Owner.create(
-#   name: "Nick Cherry"
-#   email: "ncherry7133@gmail.com"
-#   phone: "317-690-8726"
-# )
-#
-# Owner.create(
-#   name: "Chris Cox"
-#   email: "monsoonsports@me.com"
-#   phone: "317-560-9775"
-# )
-#
-# Owner.create(
-#   name: "Matt Davidson"
-#   email: "davidson@indiana.edu"
-#   phone: "317-809-5900"
-# )
-#
-# Owner.create(
-#   name: "Ryan Daley"
-#   phone: "317-508-3460"
-# )
-#
-# Owner.create(
-#   name: "Mike Dahargis"
-#   email: "mihargis@iu.edu"
-#   phone: "317-774-2448"
-# )
-#
-# Owner.create(
-#   name: "Dylan DeGraw"
-#   email: "dylan.degraw2@aol.com"
-#   phone: "765-993-3560"
-# )
-#
-# Owner.create(
-#   name: "Phil Daniels"
-#   email: "mr.philipdaniels@gmail.com"
-#   phone: "812-589-7238"
-# )
-#
-# Owner.create(
-#   name: "Landen Ferber"
-#   email: "lferber12@gmail.com"
-#   phone: "574-549-7149"
-# )
-#
-# Owner.create(
-#   name: "Kalu Ferreira"
-#   email: "kaluferreira@ymail.com"
-#   phone: "317-690-3433"
-# )
-# Owner.create(
-#   name: "Riley George"
-#   email: "george.riley51@gmail.com"
-#   phone: "765-810-3071"
-# )
-#
-# Owner.create(
-#   name: "Chris Gilbert"
-#   email: "chrisgilbert222@yahoo.com"
-#   phone: "765-247-8150"
-# )
-#
-# Owner.create(
-#   name: "Ryan Gallagher"
-#   email: "rgallagher2015@yahoo.com"
-#   phone: "440-666-4286"
-# )
-#
-# Owner.create(
-#   name: "Grant Gold"
-#   phone: "317-850-1559"
-# )
-#
-# Owner.create(
-#   name: "Cindy Harry"
-#   email: "cynthiaweissh@aol.com"
-#   phone: "317-640-1489"
-# )
-#
-# Owner.create(
-#   name: "Tannor Henvey"
-#   email: "tannorhenvey@gmail.com"
-#   phone: "260-446-5506"
-# )
-# Owner.create(
-#   name: "Tommy Hartman"
-#   phone: "317-954-6718"
-# )
-# Owner.create(
-#   name: "Kendrick Hester"
-#   email: "kendrickh07@live.com"
-#   phone: "317-796-2737"
-# )
-#
-# Owner.create(
-#   name: "Shaun Hicks"
-#   email: "shaunhicks12@icloud.com"
-#   phone: "317-266-9083"
-# )
-#
-# Owner.create(
-#   name: "Connor Ice"
-#   email: "connerice17@cchsindy.org"
-#   phone: "317-316-2020"
-# )
-#
-# Owner.create(
-#   name: "Roosevelt Jones"
-#   phone: "618-219-0158"
-# )
-#
-# Owner.create(
-#   name: "Maurice Jeter"
-#   email: "jeterm30@gmail.com"
-#   phone: "217-799-4090"
-# )
-#
-# Owner.create(
-#   name: "Rashaan Jackson"
-#   email: "rashaanjackson@gmail.com"
-#   phone: "574-527-7339"
-# )
-#
-# Owner.create(
-#   name: "Evan James"
-#   email: "evandjames14@gmail.com"
-#   phone: "317-437-7892"
-# )
-#
-# Owner.create(
-#   name: "Frankie Kozak"
-#   email: "frankiekozak@yahoo.com"
-#   phone: "317-590-0733"
-# )
-#
-# Owner.create(
-#   name: "Eric Knoll"
-#   email: "ericxknoll@gmail.com"
-#   phone: "630-344-8423"
-# )
-#
-# Owner.create(
-#   name: "Ben Levart"
-#   email: "ben.levart@gmail.com"
-#   phone: "317-737-6605"
-# )
-#
-# Owner.create(
-#   name: "Sophia Lipka"
-#   email: "sllipka@iu.edu"
-#   phone: "317-965-4156"
-# )
-#
-# Owner.create(
-#   name: "Kanji Lanham"
-#   phone: "512-695-8949"
-# )
-#
-# Owner.create(
-#   name: "Jared Levi"
-#   email: "jmlevi82@gmail.com"
-#   phone: "317-517-2344"
-# )
-#
-# Owner.create(
-#   name: "Colin Lasek"
-#   phone: "317-690-3277"
-# )
-#
-# Owner.create(
-#   name: "Faith Lanum"
-#   phone: "317-909-4945"
-# )
-#
-# Owner.create(
-#   name: "Matt McCannon"
-#   phone: "317-538-7777"
-# )
-#
-# Owner.create(
-#   name: "Roy Mathes"
-#   phone: "317-413-3912"
-# )
-#
-# Owner.create(
-#   name: "Anthony Maranto"
-#   email: "marantoa@uindy.edu"
-#   phone: "317-985-7136"
-# )
-#
-# Owner.create(
-#   name: "Mary Medina"
-#   email: "medinamary64@gmail.com"
-#   phone: "317-590-1452"
-# )
-#
-# Owner.create(
-#   name: "Teresa Moore"
-#   email: "more3four@gmail.com"
-#   phone: "317-441-3319"
-# )
-#
-# Owner.create(
-#   name: "Brandon Murray"
-#   phone: "815-992-5081"
-# )
-#
-# Owner.create(
-#   name: "Chris McNeary"
-#   email: "mcnearyk@uindy.edu"
-#   phone: "765-228-5081"
-# )
-#
-# Owner.create(
-#   name: "Ben McQueen"
-#   phone: "317-752-3275"
-# )
-#
-# Owner.create(
-#   name: "Beth Meeks"
-#   email: "bethrmeeks@yahoo.com"
-#   phone: "317-366-6590"
-# )
-#
-# Owner.create(
-#   name: "Connor Newlawn"
-#   phone: "317-313-1847"
-# )
-#
-# Owner.create(
-#   name: "Jackson Nance"
-#   email: "nancejackson0@gmail.com"
-#   phone: "260-330-9636"
-# )
-#
-# Owner.create(
-#   name: "Cahmelan Porter"
-#   email: "cahmelanporter@gmail.com"
-#   phone: "317-645-7703"
-# )
-#
-# Owner.create(
-#   name: "Paris Pyles"
-#   email: "ppyles09@gmail.com"
-#   phone: "317-384-2836"
-# )
-#
-# Owner.create(
-#   name: "Scott Prewitt"
-#   email: "scotthkp@gmail.com"
-#   phone: "317-498-2716"
-# )
-#
-# Owner.create(
-#   name: "Jacob Price"
-#   email: "jacobprice1322@gmail.com"
-#   phone: "812-249-3772"
-# )
-#
-# Owner.create(
-#   name: "Tarik Purifoy"
-# )
-#
-# Owner.create(
-#   name: "Garland Porter"
-#   email: "garlandporter@hotmail.com"
-#   phone: "317-340-0381"
-# )
-#
-# Owner.create(
-#   name: "Charles Reisner"
-# )
-#
-# Owner.create(
-#   name: "Aldo Rodriguez"
-#   phone: "317-489-7131"
-# )
-#
-# Owner.create(
-#   name: "Courtney Robinson"
-#   email: "courtneyrobinson12@gmail.com"
-#   phone: "317-681-9483"
-# )
-#
-# Owner.create(
-#   name: "David Rosenberg"
-#   email: "dtheberg@gmail.com"
-#   phone: "336-202-8956"
-# )
-#
-# Owner.create(
-#   name: "Micheal Rader"
-#   email: "mjrader@gmail.com"
-#   phone: "317-694-6841"
-# )
-#
-# Owner.create(
-#   name: "George Rodawg"
-#   phone: "217-778-4342"
-# )
-#
-# Owner.create(
-#   name: "Jackson Reilly"
-#   email: "jackreilly42@gmail.com"
-#   phone: "502-345-0681"
-# )
-#
-# Owner.create(
-#   name: "Bridgot Reid"
-#   phone: "812-528-6178"
-# )
-#
-# Owner.create(
-#   name: "Max Sarayev"
-#   email: "max.sarayev@me.com"
-#   phone: "317-987-2883"
-# )
-#
-# Owner.create(
-#   name: "Aaron Shyr"
-#   email: "aaronshyr@gmail.com"
-#   phone: "317-909-3364"
-# )
-#
-# Owner.create(
-#   name: "Charles Santos"
-#   email: "cbsantos9891@yahoo.com"
-#   phone: "317-525-2977"
-# )
-#
-# Owner.create(
-#   name: "Thomas Spottz"
-#   phone: "317-341-3004"
-# )
-#
-# Owner.create(
-#   name: "Matt Stevens"
-#   email: "stevensmatt777@gmail.com"
-#   phone: "937-733-8461"
-# )
-#
-# Owner.create(
-#   name: "Anthony Scott"
-#   email: "anthony.scott.9140@gmail.com"
-#   phone: "317-908-8053"
-# )
-#
-# Owner.create(
-#   name: "Max Silverman"
-#   email: "rdsilverman@gmail.com"
-#   phone: "217-801-5907"
-# )
-#
-# Owner.create(
-#   name: "Chaz Spicer"
-#   email: "cspice30@gmail.com"
-#   phone: "317-626-2451"
-# )
+Owner.create(
+  name: "Camden Allen",
+  email: "allen376@purdue.edu",
+  phone: "317-989-2713"
+)
+
+Owner.create(
+  name: "Jose Alvarado",
+  email: "jalvarado22@ivytech.edu",
+  phone: "317-941-5774"
+)
+
+Owner.create(
+  name: "Matheu Bragg",
+  email: "matheubragg@gmail.com",
+  phone: "513-461-5123"
+)
+
+Owner.create(
+  name: "Ryan Brown",
+  email: "ryanwbrown_2001@icloud.com",
+  phone: "317-385-6691"
+)
+
+Owner.create(
+  name: "Bailey Bolton",
+  email: "boltonbailey@yahoo.com",
+  phone: "317-970-3154"
+)
+
+Owner.create(
+  name: "Levon Brown",
+  email: "levon.brown@yahoo.com",
+  phone: "317-260-7059"
+)
+
+Owner.create(
+  name: "Andrew Bolen",
+  email: "abolsax@yahoo.com",
+  phone: "317-608-7714"
+)
+
+Owner.create(
+  name: "Brock Bishop",
+  email: "brockjbishop@gmail.com",
+  phone: "317-771-6651"
+)
+
+Owner.create(
+  name: "Dan Blazier",
+  email: "dblazier1@gmail.com",
+  phone: "618-973-9063"
+)
+
+Owner.create(
+  name: "William Coffee",
+  email: "wcoffee05@yahoo.com",
+  phone: "773-934-4770"
+)
+
+Owner.create(
+  name: "Aaron Cunningham",
+  email: "brightfuture317@gmail.com",
+  phone: "317-453-7188"
+)
+
+Owner.create(
+  name: "Courtnee Cruz",
+  email: "courtnee.cruz@gmail.com",
+  phone: "219-228-0178"
+)
+
+Owner.create(
+  name: "Nick Cherry",
+  email: "ncherry7133@gmail.com",
+  phone: "317-690-8726"
+)
+
+Owner.create(
+  name: "Chris Cox",
+  email: "monsoonsports@me.com",
+  phone: "317-560-9775"
+)
+
+Owner.create(
+  name: "Matt Davidson",
+  email: "davidson@indiana.edu",
+  phone: "317-809-5900"
+)
+
+Owner.create(
+  name: "Ryan Daley",
+  phone: "317-508-3460"
+)
+
+Owner.create(
+  name: "Mike Dahargis",
+  email: "mihargis@iu.edu",
+  phone: "317-774-2448"
+)
+
+Owner.create(
+  name: "Dylan DeGraw",
+  email: "dylan.degraw2@aol.com",
+  phone: "765-993-3560"
+)
+
+Owner.create(
+  name: "Phil Daniels",
+  email: "mr.philipdaniels@gmail.com",
+  phone: "812-589-7238"
+)
+
+Owner.create(
+  name: "Landen Ferber",
+  email: "lferber12@gmail.com",
+  phone: "574-549-7149"
+)
+
+Owner.create(
+  name: "Kalu Ferreira",
+  email: "kaluferreira@ymail.com",
+  phone: "317-690-3433"
+)
+Owner.create(
+  name: "Riley George",
+  email: "george.riley51@gmail.com",
+  phone: "765-810-3071"
+)
+
+Owner.create(
+  name: "Chris Gilbert",
+  email: "chrisgilbert222@yahoo.com",
+  phone: "765-247-8150"
+)
+
+Owner.create(
+  name: "Ryan Gallagher",
+  email: "rgallagher2015@yahoo.com",
+  phone: "440-666-4286"
+)
+
+Owner.create(
+  name: "Grant Gold",
+  phone: "317-850-1559"
+)
+
+Owner.create(
+  name: "Cindy Harry",
+  email: "cynthiaweissh@aol.com",
+  phone: "317-640-1489"
+)
+
+Owner.create(
+  name: "Tannor Henvey",
+  email: "tannorhenvey@gmail.com",
+  phone: "260-446-5506"
+)
+Owner.create(
+  name: "Tommy Hartman",
+  phone: "317-954-6718"
+)
+Owner.create(
+  name: "Kendrick Hester",
+  email: "kendrickh07@live.com",
+  phone: "317-796-2737"
+)
+
+Owner.create(
+  name: "Shaun Hicks",
+  email: "shaunhicks12@icloud.com",
+  phone: "317-266-9083"
+)
+
+Owner.create(
+  name: "Connor Ice",
+  email: "connerice17@cchsindy.org",
+  phone: "317-316-2020"
+)
+
+Owner.create(
+  name: "Roosevelt Jones",
+  phone: "618-219-0158"
+)
+
+Owner.create(
+  name: "Maurice Jeter",
+  email: "jeterm30@gmail.com",
+  phone: "217-799-4090"
+)
+
+Owner.create(
+  name: "Rashaan Jackson",
+  email: "rashaanjackson@gmail.com",
+  phone: "574-527-7339"
+)
+
+Owner.create(
+  name: "Evan James",
+  email: "evandjames14@gmail.com",
+  phone: "317-437-7892"
+)
+
+Owner.create(
+  name: "Frankie Kozak",
+  email: "frankiekozak@yahoo.com",
+  phone: "317-590-0733"
+)
+
+Owner.create(
+  name: "Eric Knoll",
+  email: "ericxknoll@gmail.com",
+  phone: "630-344-8423"
+)
+
+Owner.create(
+  name: "Ben Levart",
+  email: "ben.levart@gmail.com",
+  phone: "317-737-6605"
+)
+
+Owner.create(
+  name: "Sophia Lipka",
+  email: "sllipka@iu.edu",
+  phone: "317-965-4156"
+)
+
+Owner.create(
+  name: "Kanji Lanham",
+  phone: "512-695-8949"
+)
+
+Owner.create(
+  name: "Jared Levi",
+  email: "jmlevi82@gmail.com",
+  phone: "317-517-2344"
+)
+
+Owner.create(
+  name: "Colin Lasek",
+  phone: "317-690-3277"
+)
+
+Owner.create(
+  name: "Faith Lanum",
+  phone: "317-909-4945"
+)
+
+Owner.create(
+  name: "Matt McCannon",
+  phone: "317-538-7777"
+)
+
+Owner.create(
+  name: "Roy Mathes",
+  phone: "317-413-3912"
+)
+
+Owner.create(
+  name: "Anthony Maranto",
+  email: "marantoa@uindy.edu",
+  phone: "317-985-7136"
+)
+
+Owner.create(
+  name: "Mary Medina",
+  email: "medinamary64@gmail.com",
+  phone: "317-590-1452"
+)
+
+Owner.create(
+  name: "Teresa Moore",
+  email: "more3four@gmail.com",
+  phone: "317-441-3319"
+)
+
+Owner.create(
+  name: "Brandon Murray",
+  phone: "815-992-5081"
+)
+
+Owner.create(
+  name: "Chris McNeary",
+  email: "mcnearyk@uindy.edu",
+  phone: "765-228-5081"
+)
+
+Owner.create(
+  name: "Ben McQueen",
+  phone: "317-752-3275"
+)
+
+Owner.create(
+  name: "Beth Meeks",
+  email: "bethrmeeks@yahoo.com",
+  phone: "317-366-6590"
+)
+
+Owner.create(
+  name: "Connor Newlawn",
+  phone: "317-313-1847"
+)
+
+Owner.create(
+  name: "Jackson Nance",
+  email: "nancejackson0@gmail.com",
+  phone: "260-330-9636"
+)
+
+Owner.create(
+  name: "Cahmelan Porter",
+  email: "cahmelanporter@gmail.com",
+  phone: "317-645-7703"
+)
+
+Owner.create(
+  name: "Paris Pyles",
+  email: "ppyles09@gmail.com",
+  phone: "317-384-2836"
+)
+
+Owner.create(
+  name: "Scott Prewitt",
+  email: "scotthkp@gmail.com",
+  phone: "317-498-2716"
+)
+
+Owner.create(
+  name: "Jacob Price",
+  email: "jacobprice1322@gmail.com",
+  phone: "812-249-3772"
+)
+
+Owner.create(
+  name: "Tarik Purifoy"
+)
+
+Owner.create(
+  name: "Garland Porter",
+  email: "garlandporter@hotmail.com",
+  phone: "317-340-0381"
+)
+
+Owner.create(
+  name: "Charles Reisner"
+)
+
+Owner.create(
+  name: "Aldo Rodriguez",
+  phone: "317-489-7131"
+)
+
+Owner.create(
+  name: "Courtney Robinson",
+  email: "courtneyrobinson12@gmail.com",
+  phone: "317-681-9483"
+)
+
+Owner.create(
+  name: "David Rosenberg",
+  email: "dtheberg@gmail.com",
+  phone: "336-202-8956"
+)
+
+Owner.create(
+  name: "Micheal Rader",
+  email: "mjrader@gmail.com",
+  phone: "317-694-6841"
+)
+
+Owner.create(
+  name: "George Rodawg",
+  phone: "217-778-4342"
+)
+
+Owner.create(
+  name: "Jackson Reilly",
+  email: "jackreilly42@gmail.com",
+  phone: "502-345-0681"
+)
+
+Owner.create(
+  name: "Bridgot Reid",
+  phone: "812-528-6178"
+)
+
+Owner.create(
+  name: "Max Sarayev",
+  email: "max.sarayev@me.com",
+  phone: "317-987-2883"
+)
+
+Owner.create(
+  name: "Aaron Shyr",
+  email: "aaronshyr@gmail.com",
+  phone: "317-909-3364"
+)
+
+Owner.create(
+  name: "Charles Santos",
+  email: "cbsantos9891@yahoo.com",
+  phone: "317-525-2977"
+)
+
+Owner.create(
+  name: "Thomas Spottz",
+  phone: "317-341-3004"
+)
+
+Owner.create(
+  name: "Matt Stevens",
+  email: "stevensmatt777@gmail.com",
+  phone: "937-733-8461"
+)
+
+Owner.create(
+  name: "Anthony Scott",
+  email: "anthony.scott.9140@gmail.com",
+  phone: "317-908-8053"
+)
+
+Owner.create(
+  name: "Max Silverman",
+  email: "rdsilverman@gmail.com",
+  phone: "217-801-5907"
+)
+
+Owner.create(
+  name: "Chaz Spicer",
+  email: "cspice30@gmail.com",
+  phone: "317-626-2451"
+)
 
 # Start the T's next
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
